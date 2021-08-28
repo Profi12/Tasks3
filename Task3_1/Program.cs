f1d2691959be13291cceb453525eeba1382d3115
Larisa Samoilenko <qwry28@gmail.com>
2021-08-28 16:35:04 +0300

    Add project files.

---
+++ Task3_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Tasks3Library;
+
+namespace Task3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(TasksHelper.Pow(a, b));
+        }
+    }
+}
---
+++ Tasks3Library/TasksHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace Tasks3Library
+{
+    public class TasksHelper
+    {
+        public static int Pow(int a, int b)
+        {
+            int s = a;
+            if (b == 0)
+            {
+                s = 1;
+            }
+
+            for (int i = 1; i < b; i++)
+            {
+                s = s * a;
+            }
+
+            return s;
+        }
+    }
+}
